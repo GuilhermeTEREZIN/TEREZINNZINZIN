8283ccb5470a112c00ca791ea626991855318c88
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-05-25 15:07:34 -0300

    Update Time.java

--- PojetoBrasfoot/src/Time.java
+++ PojetoBrasfoot/src/Time.java
@@ -1,3 +1,29 @@
+import java.util.ArrayList;
+
 public class Time {
+  private int id;
   private String nome;
+  private ArrayList<Jogador> jogadores;
+
+  public Time(int id,String nome){
+    this.nome = nome;
+    this.id = id;
+    jogadores = new ArrayList<>();
+
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+  public void addJogador(Jogador j){
+    jogadores.add(j);
+  }
 }
