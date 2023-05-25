57791ba31086cf813bb922ffddbb7b6f2b317430
RibeiroEtk <gabrielestefanski7@gmail.com>
2023-05-25 19:20:13 -0300

    oi

--- PojetoBrasfoot/src/Campeonato.java
+++ PojetoBrasfoot/src/Campeonato.java
@@ -1,2 +1,18 @@
-public class Campeonato {
+import java.util.ArrayList;
+
+public abstract class Campeonato {
+    private String nome;
+    private ArrayList<Time> times;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Time> getTimes() {
+        return times;
+    }
+    public Time getTime(int id){
+        return times.get(id);
+
+    }
 }
--- PojetoBrasfoot/src/Partida.java
+++ PojetoBrasfoot/src/Partida.java
@@ -1,13 +1,23 @@
 public class Partida {
     private Time time1;
     private Time time2;
-    
+
 
     public Partida(Time time1,Time time2){
         this.time1 = time1;
         this.time2 = time2;
     }
 
-    public void simularPartida();
+    public Time getTime1() {
+        return time1;
+    }
+
+    public Time getTime2() {
+        return time2;
+    }
+
+    public void simularPartida(){
+        System.out.println("oi");
+    }
 
 }
