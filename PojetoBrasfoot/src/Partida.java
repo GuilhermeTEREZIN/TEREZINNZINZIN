6fbd65704057e80bb844a3d346ddcf3f2ebf430a
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-05-30 14:39:48 -0300

    .

--- PojetoBrasfoot/src/Partida.java
+++ PojetoBrasfoot/src/Partida.java
@@ -1,24 +1,30 @@
+import java.util.Random;
+
 public class Partida {
     private Time time1;
     private Time time2;
-
 
     public Partida(Time time1,Time time2){
         this.time1 = time1;
         this.time2 = time2;
     }
-
     public Time getTime1() {
         return time1;
     }
-
     public Time getTime2() {
         return time2;
     }
 
     public void simularPartida(){
+        Random random = new Random();
         System.out.println("------------------------------------------");
         System.out.println(time1.getNome()+" X "+ time2.getNome() );
+        for(int fp = 0;fp<=45;fp++){
+            if(random(10) == 10){
+                System.out.println("Oi");
+            }
+
+        }
         
 
     }
