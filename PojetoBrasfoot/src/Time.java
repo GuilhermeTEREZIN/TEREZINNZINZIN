22df28a718956092af96f504bacd45e357d9cf7a
Bruno Guttervill <bguttervill@gmail.com>
2023-06-01 17:52:00 -0300

    Update Time.java

--- PojetoBrasfoot/src/Time.java
+++ PojetoBrasfoot/src/Time.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import static java.lang.Math.round;
+
 public class Time {
   private int id;
   private String nome;
@@ -26,5 +28,49 @@ public class Time {
   public void addJogador(Jogador j){
     jogadores.add(j);
   }
+  public int over (ArrayList<Jogador> jogadores){
+    int Over=0;
+    int Contador=0;
+    for(Jogador j:jogadores){
+      Over+=j.getOverall();
+      Contador += 1;
+    }
+    return round(Over/Contador);
+
+  }
+  public int overDefesa (ArrayList<Jogador> jogadores){
+    int Over=0;
+    int Contador=0;
+    for(Jogador j:jogadores){
+      if (j.getPosicao()=="ZAG"|| j.getPosicao()=="GO"|| j.getPosicao()=="LD"|| j.getPosicao()=="LE"){
+        Over+=j.getOverall();
+        Contador += 1;}
+    }
+    return round(Over/Contador);
+
+  }
+  public int overMeio (ArrayList<Jogador> jogadores){
+    int Over=0;
+    int Contador=0;
+    for(Jogador j:jogadores){
+      if (j.getPosicao()=="VOL"|| j.getPosicao()=="MLG"|| j.getPosicao()=="MEI"){
+        Over+=j.getOverall();
+        Contador += 1;}
+    }
+    return round(Over/Contador);
+
+  }
+  public int overAtaque (ArrayList<Jogador> jogadores){
+    int Over=0;
+    int Contador=0;
+    for(Jogador j:jogadores){
+      if (j.getPosicao()=="CA"|| j.getPosicao()=="PTE"|| j.getPosicao()=="PTD"){
+        Over+=j.getOverall();
+        Contador += 1;}
+    }
+    return round(Over/Contador);
+
+  }
+
 
 }
