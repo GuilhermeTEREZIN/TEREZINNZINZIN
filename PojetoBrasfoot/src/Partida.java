f1c54f500e6f5af6189fce67f5187c979e5ed3f3
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-01 19:46:07 -0300

    oi

--- PojetoBrasfoot/src/Partida.java
+++ PojetoBrasfoot/src/Partida.java
@@ -10,33 +10,59 @@ public class Partida {
         int t2gols = 0;
         int t1 = 0;
         int t2 = 0;
+        int chance;
         System.out.println("------------------------------------------");
         System.out.println(time1.getNome()+" X "+ time2.getNome() );
         int x = 0;
         for(int i = 0;i<=90;i++){
             x = r.nextInt(5);
             if(x == 0){
+
                 t1 = r.nextInt(100);
                 t2 = r.nextInt(100);
                 if (t1> t2){ //chance do time 1
-                    int chance = r.nextInt(100);
-                    if (chance<= 20){
+                    chance = r.nextInt(100);
+                    if (chance<= 20){  //fez o gol
+                        t1gols +=1;
+
+                        System.out.println(i+"' - "+"Gol do "+time1.getNome());
+                        System.out.println(time1.getNome()+" "+t1gols+" X "+t2gols+" "+time2.getNome());
 
                     }else{
                         if (r.nextInt(5)==0){ // chance de contra ataque
-
+                            chance = r.nextInt();
+                            if(chance<=20){ // gol de contra ataque
+                                t2gols += 1;
+                                System.out.println(i+"' - "+"Gol de contra ataque do "+time2.getNome());
+                                System.out.println(time1.getNome()+" "+t1gols+" X "+t2gols+" "+time2.getNome());
+                            }
                         }
                     }
                 } else if (t2>t1) {//chance do time 2
-
+                    chance = r.nextInt(100);
+                    if (chance<= 20){  //fez o gol
+                        t2gols +=1;
+                        System.out.println(i+"' - "+"Gol do "+time2.getNome());
+                        System.out.println(time1.getNome()+" "+t1gols+" X "+t2gols+" "+time2.getNome());
+                    }else{
+                        if (r.nextInt(5)==0){ // chance de contra ataque
+                            chance = r.nextInt();
+                            if(chance<=20){ // gol de contra ataque
+                                t1gols += 1;
+                                System.out.println(i+"' - "+"Gol de contra ataque do "+time1.getNome());
+                                System.out.println(time1.getNome()+" "+t1gols+" X "+t2gols+" "+time2.getNome());
+                            }
+                        }
+                    }
                 }
-                System.out.println(i+"' - ");
+
 
             }
 
         }
 
-
+        System.out.println("\nResultado final:");
+        System.out.println(time1.getNome()+" "+t1gols+" X "+t2gols+" "+time2.getNome());
     }
 
 }
