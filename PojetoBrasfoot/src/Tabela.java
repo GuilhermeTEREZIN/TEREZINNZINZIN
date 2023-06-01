2f1f3b6f96b2fa8deecee15dcd011e33ab42132c
Giovanni Lucca Reque Uchoa <gi.lucca10@gmail.com>
2023-06-01 19:49:34 -0300

    gets and sets interface tabela

--- PojetoBrasfoot/src/Tabela.java
+++ PojetoBrasfoot/src/Tabela.java
@@ -1,3 +1,11 @@
 public interface Tabela {
+    String getNome();
+    String setNome();
+    int getPontos();
+    int setPontos();
+    int getGolsfeitos();
+    int setGolsfeitos();
+    int getGolssofridos();
+    int setGolssofridos();
 
 }
