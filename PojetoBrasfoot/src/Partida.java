9a27f844ffcb48d97c49c408135f93c16a6dc9cb
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-01 17:59:10 -0300

    Update Partida.java

--- PojetoBrasfoot/src/Partida.java
+++ PojetoBrasfoot/src/Partida.java
@@ -10,17 +10,24 @@ public class Partida {
 //    }
 //    public static int calcularForcaDefesa(Time time){
 //        return round((time.getOverAtk+time.getOveMid())/2);
-//    }
+//
 
     public static void simularPartida(Time time1, Time time2){
         Random r = new Random();
+        int t1gols = 0;
+        int t2gols = 0;
+        int t1 = 0;
+        int t2 = 0;
         System.out.println("------------------------------------------");
         System.out.println(time1.getNome()+" X "+ time2.getNome() );
         int x = 0;
-        for(int i = 0;i<=45;i++){
+        for(int i = 0;i<=90;i++){
             x = r.nextInt(5);
             if(x == 0){
-                System.out.println(x);
+                t1 = r.nextInt(100);
+                t2 = r.nextInt(100);
+                System.out.println(i+"' - ");
+
             }
 
         }
