621d90973937ba2581b40f2b28dd36a4c9e4023f
GuilhermeTEREZIN <gui158gta@hotmail.com>
2023-06-06 17:11:08 -0300

    importar tabela jogadores
    gui terezin

--- PojetoBrasfoot/src/importarCsv.java
+++ PojetoBrasfoot/src/importarCsv.java
@@ -24,32 +24,32 @@ public class importarCsv
 
     }
 
-//    public static ArrayList<Jogador> jogadores() throws Exception
-//    {
-//
-//        ArrayList<Jogador>  jogadores = new ArrayList<>(); // cria uma lista de jogadores
-//        String nomeJogador[];//Variavel para auxiliar na extração dos dados
-//
-//        File file = new File("./PojetoBrasFoot/base/tabelajogadorescsvnova.CSV"); // abre o arquivo csv
-////        System.out.println(file.exists());
-//        Scanner sc = new Scanner(file);//inicia a classe scanner
-//        // sc.useDelimiter(",");   //sets the delimiter pattern
-//        while (sc.hasNext())  //returns a boolean value
-//        {
-//            nomeJogador = sc.next().split(","); // Separa o id do nome do time
-//            //nomeTime[0] = id
-//            //nomeTime[1] = nome
-//            //nomeTime[2] = idade
-//            //nomeTime[3] = clube
-//            //nomeTime[4] = posicao
-//            //nomeTime[5] = numerocamisa
-//            //nomeTime[6] = overral
-//            //nomeTime[7] = nacionalidade
-//
-//            jogadores.add(new Jogador(Integer.parseInt(nomeJogador[0].strip()),nomeJogador[1].strip()));//adciona o time a lista de times
-//        }
-//        sc.close();  //closes the scanner
-//        return jogadores; //retorna a lista de times
-//
-//    }
+    public static ArrayList<Jogador> jogadores() throws Exception
+    {
+
+        ArrayList<Jogador>  jogadores = new ArrayList<>(); // cria uma lista de jogadores
+        String nomeJogador[];//Variavel para auxiliar na extração dos dados
+
+        File file = new File("./PojetoBrasFoot/base/tabelajogadorescsvnova.CSV"); // abre o arquivo csv
+//        System.out.println(file.exists());
+        Scanner sc = new Scanner(file);//inicia a classe scanner
+        // sc.useDelimiter(",");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            nomeJogador = sc.next().split(","); // Separa o id do nome do time
+            //nomeTime[0] = id
+            //nomeTime[1] = nome
+            //nomeTime[2] = idade
+            //nomeTime[3] = clube
+            //nomeTime[4] = posicao
+            //nomeTime[5] = numerocamisa
+            //nomeTime[6] = overral
+            //nomeTime[7] = nacionalidade
+
+            jogadores.add(new Jogador(Integer.parseInt(nomeJogador[0].strip()),nomeJogador[1].strip(),nomeJogador[2].strip(),nomeJogador[3].strip(),nomeJogador[4].strip(),nomeJogador[5].strip(),nomeJogador[6].strip(),nomeJogador[7].strip()));//adciona o time a lista de times
+        }
+        sc.close();  //closes the scanner
+        return jogadores; //retorna a lista de times
+
+    }
 }
