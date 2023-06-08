fd6d91853ee35a30504dd1118506b7323de1e17c
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-06-07 22:01:01 -0300

    oi1

--- PojetoBrasfoot/src/Liga.java
+++ PojetoBrasfoot/src/Liga.java
@@ -1,19 +1,19 @@
-//import java.util.ArrayList;
-//
-//public class Liga implements Tabela{
-//    String[][] tabela = new String[20][6];
-//    //id - nome - pontos - jogos - golsfeitos - golsofridos
-//    //importarCsv.times() - tras a lista completa dos times
-//    public Liga(){
-//    }
-//    String getNome(int id);
-//    void setNome(int id);
-//    int getPontos(int id);
-//    void setPontos(int id);
-//    int getGolsfeitos(int id);
-//    void setGolsfeitos(int id);
-//    int getGolssofridos(int id);
-//    void setGolssofridos(int id);
-//    void setJogos(int id);
-//    int getJogos(int id);
-//}
+import java.util.ArrayList;
+
+public class Liga implements Tabela {
+    String[][] tabela = new String[20][6];
+    //id - nome - pontos - jogos - golsfeitos - golsofridos
+    //importarCsv.times() - tras a lista completa dos times
+    public Liga(){
+    }
+    String getNome(int id);
+    void setNome(int id);
+    int getPontos(int id);
+    void setPontos(int id);
+    int getGolsfeitos(int id);
+    void setGolsfeitos(int id);
+    int getGolssofridos(int id);
+    void setGolssofridos(int id);
+    void setJogos(int id);
+    int getJogos(int id);
+}
