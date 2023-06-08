8ef77748a60ca999de0f2410b5c1b1431d198503
GuilhermeTEREZIN <gui158gta@hotmail.com>
2023-06-07 22:13:59 -0300

    tabela campeonato
    gui terezin

--- PojetoBrasfoot/src/importarCsv.java
+++ PojetoBrasfoot/src/importarCsv.java
@@ -21,6 +21,26 @@ public class importarCsv {
         return times; //retorna a lista de times
 
     }
+    public static ArrayList<Time> tabela() throws Exception {
+        ArrayList<Time> times = new ArrayList<>(); // cria uma lista de times
+        String nomeTime[];//Variavel para auxiliar na extração dos dados
+
+        File file = new File("./PojetoBrasFoot/save/Liga.csv"); // abre o arquivo csv
+//        System.out.println(file.exists());
+        Scanner sc = new Scanner(file);//inicia a classe scanner
+        // sc.useDelimiter(",");   //sets the delimiter pattern
+        while (sc.hasNext())  //returns a boolean value
+        {
+            nomeTime = sc.next().split(","); // Separa o id do nome do time
+            //nomeTime[0] = id
+            //nomeTime[1] = nome do time
+            times.add(new Time(Integer.parseInt(nomeTime[0].strip()), nomeTime[1].strip()));//adciona o time a lista de times
+        }
+        sc.close();  //closes the scanner
+        return times; //retorna a lista de times
+
+    }
+
 
     public static ArrayList<Jogador> jogadores() throws Exception {
 
@@ -96,5 +116,6 @@ public class importarCsv {
 
         }
     }
+
 
 }
