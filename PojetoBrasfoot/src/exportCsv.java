0e433c616d8e123d2526e96736d2124e5425d158
GuilhermeTEREZIN <gui158gta@hotmail.com>
2023-06-10 16:25:17 -0300

    add export codigo
    gui terezin

--- PojetoBrasfoot/src/exportCsv.java
+++ PojetoBrasfoot/src/exportCsv.java
@@ -1,2 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
 public class exportCsv {
+    public static void main(String[] args) {
+        try {
+            FileWriter arq = new FileWriter("./PojetoBrasFoot/save/save.csv");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Digite algo: ");
+            String entrada = scanner.nextLine();
+
+            arq.write(entrada + "\n");
+            arq.write("abre a perna que la vem pika ...");
+            arq.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
