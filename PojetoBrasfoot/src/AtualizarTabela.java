66f1b1790fe2a51788e7350f0de88100ff3a1eb7
GuilhermeTEREZIN <gui158gta@hotmail.com>
2023-06-12 16:14:01 -0300

    gui
    aaaa

---
+++ PojetoBrasfoot/src/AtualizarTabela.java
@@ -0,0 +1,14 @@
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//public class AtualizarTabela {
+//    exportTimes(Player p)
+//        for(Time t:p.getTimes){
+//        ////escreve no arquivo
+//        p.getNome,p.getPontos,p.getJogos,........
+//    }
+//}
